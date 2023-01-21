ec2ca02aac9a91389b68856ba0675c69a5d32ca9
tinchods93 <mds.designss@gmail.com>
2023-01-21 00:41:10 -0300

    fix: olvide de eliminar comentarios

--- retos/Reto-2/martinds93/index.ts
+++ retos/Reto-2/martinds93/index.ts
@@ -1,5 +1,3 @@
-// import { assert } from 'chai';
-import assert from 'node:assert/strict';
 const regex = /[^A-ZÁ-ÿ-a-z0-9]/gi;
 
 const normalizeInput = (input: string): string =>
@@ -8,7 +6,7 @@ const normalizeInput = (input: string): string =>
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '');
 
-const palindrome = (input: string): string => {
+const palindromeMds = (input: string): string => {
   const tipo = input.includes(' ') ? 'frase' : 'palabra';
   const _input = normalizeInput(input).toLowerCase();
 
@@ -16,10 +14,7 @@ const palindrome = (input: string): string => {
   return `La ${tipo} ${output === _input ? 'es' : 'no es'} un palíndromo`;
 };
 
-assert.deepEqual(
-  palindrome('¿Acaso hubo búhos acá?'),
-  'La frase es un palíndromo'
-);
-assert.deepEqual(palindrome('Ana'), 'La palabra es un palíndromo');
-assert.deepEqual(palindrome('Frase random'), 'La frase no es un palíndromo');
-assert.deepEqual(palindrome('Hola'), 'La palabra no es un palíndromo');
+const inputPorComando = process.argv[2];
+console.log(inputPorComando, '==>', palindromeMds(inputPorComando));
+
+export default palindromeMds;
