13eeee374ee0d65043fc410ed4b636faa228caff
loxi1 <anibal.cayetano@gmail.com>
2023-01-21 20:53:03 -0500

    Actualización retos

--- retos/Reto-1/Loxi1/fibonacci.js
+++ retos/Reto-1/Loxi1/fibonacci.js
@@ -1,27 +1,32 @@
 let arrayFibo = []
 let cantDeIteraciones = 0
-let axu = 0
+let aux = 0
 
 const formarArray = (cantDeIteraciones) => {
     for (let index = 1; index <= cantDeIteraciones; index++) {
-        if(index<=2)
-            axu++
-        else
-            aux = cantDeIteraciones[index-1] + cantDeIteraciones[index-2]
-        cantDeIteraciones.push(aux)
+        if (index<=2) {
+            aux++
+        } else {
+            aux = arrayFibo[index-2] + arrayFibo[index-3]
+        }
+        arrayFibo.push(aux)
     }
-    return cantDeIteraciones
+    return arrayFibo
 }
 
 const mostrarFibo = (indice) => {
-    if(indice<=0)
-        if(indice<0)
-        arrayFibo.push("Ingresar # mayor o igual a 0");
-        else
+    if(indice<=0) {
+        if (indice<0) {
+            arrayFibo.push("Ingresar # mayor o igual a 0");
+        }
+        else {
             arrayFibo.push(1)
+        }
+    }
     return formarArray(indice)
 }
 
-cantDeIteraciones=0
+cantDeIteraciones=10
 let rta = mostrarFibo(cantDeIteraciones)
-console.log(rta.toString())
+console.clear();
+console.log(`\nPara la cantidad de iteraciones: ${cantDeIteraciones}\n${rta.toString()}\n`)
--- retos/Reto-2/Loxi1/polimorfismo.js
+++ retos/Reto-2/Loxi1/polimorfismo.js
@@ -1,29 +1,33 @@
-let arrayParrafo = [], arrayReverso = [], tipoTexto = []
-let axu = '', esTexto = false, _palabraTexto = '', texto = ''
+let arrayParrafo = []
+let arrayReverso = []
+let tipoTexto = []
+let aux = ''
+let esTexto = false
+let _palabraTexto = ''
+let texto = ''
 
-//Texto en minusculas
+// Texto en minusculas
 const convertirMinusculas = (texto) => texto.toLowerCase()
-//Quitar espacios en blanco
+// Quitar espacios en blanco
 const quitarEspacios = (texto) => texto.replace(/\s+/g, '')
-//Quitar tildes
-let quitarTides = (texto) => texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-//Quitar signos
-let quitarSignos = (texto) => texto.replace(/[¿!¡;,:\.\?#@()~"]/g,"")
+// Quitar tildes
+let quitarTides = (texto) => texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+// Quitar signos
+let quitarSignos = (texto) => texto.replace(/[¿!¡;,:\.\?#@()~"]/g,'')
 
-let esTextoPoliformico = (texto) => {
+const esTextoPoliformico = (texto) => {
     aux = convertirMinusculas(texto)
-    aux = quitarEspacios(aux)
     aux = quitarTides(aux)
+    aux = quitarEspacios(aux)
     aux = quitarSignos(aux)
 
-    //convertir en array el texto
+    // convertir en array el texto
     arrayParrafo = aux.split('')
-    //invertir el orden del array
+    // invertir el orden del array
     arrayReverso = aux.split('').reverse()
-    //Comparar el valor del array
+    // Comparar el valor del array
     arrayParrafo.every((_letra, _indc) => {
-        if(_letra != arrayReverso[_indc])
-        {
+        if (_letra != arrayReverso[_indc]) {
             esTexto = true
             return false
         }
@@ -35,6 +39,7 @@ let esTextoPoliformico = (texto) => {
     
     return tipoTexto.join(' ')
 }
+
 texto = '¿Acaso hubo búhos acá?'
 _palabraTexto = esTextoPoliformico(texto)
 console.log(`\nLa palabra:\n ${texto} \n ${_palabraTexto}\n`)
