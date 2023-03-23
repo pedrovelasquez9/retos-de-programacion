8c5447b0210db71771390cbaf2f091c8037a9aa3
Jetmarte <obcollazos@gmail.com>
2023-03-23 04:07:17 -0400

    Reto-1 en java

---
+++ retos/Reto-1/Jetmarte/Fibonacci.java
@@ -0,0 +1,22 @@
+class Fibonacci{
+
+    public static void main(String[] args){
+        secuenceFibonacci(12);
+    }
+
+    private static void secuenceFibonacci(int n){
+        for(int i=1; i< n +1; i++){
+           System.out.print(" " + numberFibonacci(i)); 
+        }
+        System.out.println("");
+    }
+
+    private static int numberFibonacci(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }else{
+            return numberFibonacci(n-1) + numberFibonacci(n-2) ; 
+        }
+        
+    }
+}
