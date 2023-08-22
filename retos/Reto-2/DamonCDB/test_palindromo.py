4617735732feab4b811e8e4df15da7f50b9e848a
DamonCDB <101036086+DamonCDB@users.noreply.github.com>
2023-08-22 10:48:12 +0200

    Add files via upload

--- retos/Reto-2/DamonCDB/test_palindromo.py
+++ retos/Reto-2/DamonCDB/test_palindromo.py
@@ -6,7 +6,7 @@ class Tests(unittest.TestCase):
         self.assertTrue(palindromo.palindrome("Anna"))
         self.assertTrue(palindromo.palindrome("Hola"))
         self.assertTrue(palindromo.palindrome("Frase random"))
-        self.assertTrue(palindromo.palindrome("¿Acaso hubo buhos aca?"))
+        self.assertTrue(palindromo.palindrome("¿Acaso hubo búhos acá?"))
 
 if __name__ == '__main__':
     unittest.main()
