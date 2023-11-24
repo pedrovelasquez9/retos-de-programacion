5ae80bb11b2f9c8d2289e89e93de96265de5b3bc
Miquel <mgonzalez@tcsa.es>
2023-11-24 01:12:55 +0000

    Implementación en c# con tests

---
+++ retos/Reto-8/miquelcie/StatiticsCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using StatisticsCalculator;
+
+Console.Write($"Introduce la cantidad de números: ");
+int cantidad = Convert.ToInt32(Console.ReadLine());
+
+List<int> numbers = new List<int>();
+for (int i = 0; i < cantidad; i++)
+{
+    Console.Write($"Número {i+1} de {cantidad}: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    numbers.Add(number);
+
+}
+
+
+int media = StatiticsCaluculator.GetMedia(numbers);int mediana = StatiticsCaluculator.GetMediana(numbers);
+List<int> moda = StatiticsCaluculator.GetModa(numbers);
+
+Console.WriteLine($"Media: {media}");
+Console.WriteLine($"Mediana: {mediana}");
+Console.WriteLine($"Moda: [{string.Join(',',moda)}]");
+
+
---
+++ retos/Reto-8/miquelcie/StatiticsCalculator/StatiticsCaluculator.cs
@@ -0,0 +1,88 @@
+﻿namespace StatisticsCalculator
+{
+    public class StatiticsCaluculator
+    {
+        /// <summary>
+        ///  La media aritmética es el promedio de todos los números en la lista. Para calcularla, sumarás todos los números en la lista y dividirás esa suma por la cantidad total de números en la lista.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int GetMedia(List<int> numbers)
+        {
+            if (numbers==null)
+                throw new ArgumentNullException("La lista de números no pude ser nula");
+
+            if (numbers.Count==0)
+                throw new ArgumentOutOfRangeException("La lista no puede estar vacía");
+
+           return numbers.Sum() / numbers.Count;            
+
+        }
+        /// <summary>
+        ///  La mediana es el valor que se encuentra en el medio de la lista cuando los números están ordenados de menor a mayor. Para calcularla, primero ordenarás la lista y luego, si la cantidad de números en la lista es impar, tomarás el valor en el medio. Si la cantidad es par, tomarás el promedio de los dos valores del medio.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static int GetMediana(List<int> numbers)
+        {
+
+            if (numbers == null)
+                throw new ArgumentNullException("La lista de números no pude ser nula");
+
+            if (numbers.Count == 0)
+                throw new ArgumentOutOfRangeException("La lista no puede estar vacía");
+
+            List<int> numbersOrdered = numbers.Order().ToList();
+            int mitad = numbersOrdered.Count / 2;
+            
+            if (numbersOrdered.Count % 2 == 0)
+            {
+                return (numbersOrdered[mitad - 1] + numbersOrdered[mitad]) / 2;
+            }
+            
+            return numbersOrdered[mitad];
+            
+        }
+
+        /// <summary>
+        /// La moda es el valor (o valores) que más se repite en la lista. Para calcularla, contarás cuántas veces aparece cada número en la lista y encontrará el número o números que tienen la mayor frecuencia de aparición.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static List<int> GetModa(List<int> numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException("La lista de números no pude ser nula");
+
+            if (numbers.Count == 0)
+                throw new ArgumentOutOfRangeException("La lista no puede estar vacía");
+
+            
+            List<int> moda = new List<int>();
+
+            int max = 0;
+            foreach(int number in numbers)
+            {
+                int numRepetited = numbers.Count(x => x == number);
+                if (numRepetited > max)
+                {
+                    max = numRepetited;
+                    moda.Clear();
+                    moda.Add(number);
+                }
+                else if (numRepetited == max && !moda.Contains(number))
+                {
+                    moda.Add(number);
+                }
+            }
+
+            return moda;
+        }
+    }
+}
